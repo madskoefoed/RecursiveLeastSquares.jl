ebb758b2642c8241117bfa8724beff7cf5b879fe
Mads Koefoed <koefoedmads@gmail.com>
2021-08-05 15:27:44 +0200

    Added expanding basis
    The function now explicit (rather than as before, implicitly) uses an ever-expanding window as a budget. Furthermore, jitter has been added to KRLS.

--- example/example.jl
+++ example/example.jl
@@ -1,16 +1,17 @@
 σ = 0.1;
 # Training
-t = 100;
+t = 200;
 x = collect(1:t)/t;
 b = -0.25;
 z = sin.(b * x * t);
 y = z + rand(Normal(0.0, σ), t);
 # Kernel
-k = RBF(2.0^-4, 2.0^2);
+k_krls = RBF(2.0^-5, 2.0^2);
+k_gp   = RBF(2.0^-8, 2.0^8);
 
-m_rls  = RLS(y, x, 0.99);
-m_krls = KRLS(y, x, k, 0.99, sqrt(0.1));
-m_gp   = GP(y, x, x, k, sqrt(0.1));
+m_rls  = RLS(y, x, 0.98);
+m_krls = KRLS(y, x, k_krls, t, 0.98, sqrt(0.1));
+m_gp   = GP(y, x, x, k_gp, sqrt(0.1));
 
 using Plots
 scatter(x, y, label = "Measurement", color = "grey")
@@ -19,8 +20,9 @@ plot!(x, z, label = "Signal", color = "black")
 plot!(x, m_krls.predictions, label = "KRLS", color = "green")
 plot!(x, m_gp.predictions, label = "GP", color = "red")
 
-findmin([sum((y .- KRLS(y, x, RBF(2.0^i, 2.0^j), 0.99, sqrt(0.25)).predictions).^2) for i = -10:8, j = -10:8])
+findmin([sum((y .- KRLS(y, x, RBF(2.0^i, 2.0^j), 0.98, sqrt(0.25)).predictions).^2) for i = -10:8, j = -10:8])
+findmin([sum((y .- GP(y, x, x, RBF(2.0^i, 2.0^j), sqrt(0.25)).predictions).^2) for i = -10:8, j = -10:8])
 
-plot([sqrt(mean((y .- m_krls.predictions)[1:i].^2)) for i in 1:t])
-plot!([sqrt(mean((y .- m_gp.predictions)[1:i].^2)) for i in 1:t])
+plot([sqrt(mean((y .- m_krls.predictions)[i-100:i].^2)) for i in 101:t])
+plot!([sqrt(mean((y .- m_gp.predictions)[i-100:i].^2)) for i in 101:t])
 hline!([σ])
--- src/KRLS.jl
+++ src/KRLS.jl
@@ -1,40 +1,55 @@
 function KRLS(y::FIVector,
               x::FIMatrix,
               kernel_struct::Kernel,
-              λ::FI = 0.99,
-              s2n::FI = 1.0)
+              M::Integer = size(y),
+              λ::FI = 1,
+              s2n::FI = 1,
+              jitter::AbstractFloat = 1e-8)
 
     @assert λ <= 1 && λ > 0 "Forgetting factor, λ, must be ]0;1]."
+    @assert M <= length(y) && M > 0 "Budget size, M, must be [1;T]."
 
-    T, D = size(x)
-    pred = zeros(T)
-    σ²   = zeros(T)
-    LL   = zeros(T)
+    T  = length(y)
+    ŷ  = zeros(T)
+    σ² = zeros(T)
+    basis = Int[]
+    push!(basis, 1)
+    xb = x[basis, :]
 
     # Initialization
-    xt = reshape(x[1, :], 1, D)
-    k̄ = kernel(xt, kernel_struct)[1] # scalar
-    μ = [y[1] * k̄]                   # vector
-    Σ = k̄ - k̄^2 / (s2n + k̄)          # scalar
-    Q = 1/k̄                          # scalar
+    ktt = kernel(x[[1], :], kernel_struct)[1] # scalar
+    ktt = ktt + jitter
+    μ = [y[1] * ktt]  / (s2n + ktt)           # predictive mean at time 1
+    Σ = ktt - ktt^2 / (s2n + ktt)             # scalar
+    Q = 1/ktt                                 # scalar
+    σ²[1] = y[1]^2 / (s2n + ktt) + s2n        # predictive variance at time 1
 
     for t in 2:T
-
-        xt = reshape(x[t, :], 1, D)
         # Kernel
-        k̄ = kernel(xt, kernel_struct)[1]
-        k = kernel(x[1:t-1, :], xt, kernel_struct)
+        ktt = kernel(x[[t], :], kernel_struct)[1] + jitter
+        kbt = kernel(xb, x[[t], :], kernel_struct)
 
-        # Predict
-        q = vec(Q * k)
-        h = vec(Σ * q)
+        # Forget
+        # B2P - Back 2 Prior
+        #Σ = Σ * λ .+ kernel(xb, kernel_struct) * (1 - λ)
+        #μ = sqrt(λ) * μ
+        # Uncertainty injection
+        #Σ = Σ / λ
+
+        # Predictive mean
+        q = vec(Q * kbt)
         ȳ = dot(q, μ)
-        γ² = k̄ - dot(k, q)
+
+        # Predictive variance
+        γ² = ktt - dot(kbt, q)
+        #γ² < 0.0 && (γ² = jitter) # Ensure that gamma squared is not negative
+        h = vec(Σ * q)
         s2f = γ² + dot(q, h)
+        #s2f < 0.0 && (s2f = jitter) # Ensure that s2f is not negative
         s2y = s2n + s2f
-        pred[t] = ȳ
-        σ²[t]  = s2y
-        #LL[t] = logpdf(Normal(pred[t], s2y), y[t])
+
+        ŷ[t]  = ȳ
+        σ²[t] = s2y
 
         # Get posteriors of N(f|μ, Σ)
         push!(μ, ȳ)
@@ -45,15 +60,19 @@ function KRLS(y::FIVector,
         s = vcat(q, [-1.0])
         Q = hcat(vcat(Q, zeros(1, t-1)), zeros(t, 1)) + s * s' / γ²
         
+        # Dictionary update
+        push!(basis, t)
+        xb = x[basis, :]
     end
-    LL = cumsum(LL) ./ (1:T)
-    return (predictions = pred, variances = σ², loglikelihood = LL)
+    return (predictions = ŷ, variances = σ², basis = basis)
 end
 
 function KRLS(y::FIVector,
               x::FIVector,
               kernel_struct::Kernel,
-              λ::FI = 0.99,
-              s2n::FI = 1.0)
-    KRLS(y, reshape(x, :, 1), kernel_struct, λ, s2n)
+              M::Integer = size(y),
+              λ::FI = 1,
+              s2n::FI = 1,
+              jitter::AbstractFloat = 1e-8)
+    KRLS(y, reshape(x, :, 1), kernel_struct, M, λ, s2n, jitter)
 end
